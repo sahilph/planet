e686c8fd194d8d4eddfeb73012edf90d804d301b
amanuely <aafework@middlebury.edu>
2017-12-20 11:05:50 -0500

    cleaned up

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -11,37 +11,82 @@ import { MatRadioModule , MatFormFieldModule, MatButtonModule, MatInputModule } 
 })
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
-   educationLevel: Array<any>= [ 1, 2, 3, 4, 5, 6 , 7, 8, 9, 11, 12, 'Higher' ];
+  educationLevel: Array<any>= [ 1, 2, 3, 4, 5, 6 , 7, 8, 9, 11, 12, 'Higher' ];
+  RegistrationMsg: String;
 
   constructor(
     private couchService: CouchService,
     private fg: FormBuilder
     ) {
-     this.registerForm = fg.group({
-       firstName: [ '', Validators.required ],
-       middleName: [ '', Validators.required ],
-       lastName: [ '', Validators.required ],
-       login: [ '', Validators.required ],
-       email: [ '', Validators.required ],
-       password: [ '', Validators.required ],
-       repeatPassword: [ '', Validators.required ],
-       language: [ '', Validators.required ],
-       phoneNumber: [ '', Validators.required ],
-       birthDay: [ '', Validators.required ],
-       gender: [ '', Validators.required ],
-       level: [ '', Validators.required ],
-       community: [ '', Validators.required ],
-       region: [ '', Validators.required ],
-       nation: [ '', Validators.required ],
-
-     });
-
+    this.createform();
   }
 
   ngOnInit() {
   }
   registerUser(userInfo: any ) {
-    console.log(userInfo);
+    this.RegistrationMsg = '';
+    if (userInfo.password === userInfo.repeatPassword) {
+      this.RegistrationMsg = '';
+      this.checkAdminExistence().then((noAdmin) => {
+        if (noAdmin) {
+          console.log('admin');
+          this.createAdmin(userInfo);
+        } else {
+          console.log('not admin');
+          this.createrecord(userInfo);
+        }
+      });
+    }else {
+      this.RegistrationMsg = 'Passwords do not Match!';
+    }
   }
 
+  createrecord(userData: any) {
+    const username: string = userData.login;
+    const password: string = userData.password;
+    this.couchService.put('_users/org.couchdb.user:' + name, { 'name': username, 'password': password, 'userData': userData, 'roles': [], 'type': 'user' })
+    .then((data) => {
+      this.RegistrationMsg = 'Your registration is successful';
+    }, (error) => {
+      this.RegistrationMsg = 'Sorry, something went wrong';
+    });
+  }
+
+  checkAdminExistence() {
+    return this.couchService.get('_users/_all_docs')
+    .then((data) => {
+    return true; // user can see data so there is no admin
+  }, (error) => {
+    return false; // user doesn't have permission so there is an admin
+  });
+  }
+
+  createAdmin(userData) {
+    this.couchService.put('_node/nonode@nohost/_config/admins/' + userData.login, userData.password).then((data) => {
+      this.RegistrationMsg = 'Your registration is successful';
+    }, (error) => {
+      this.RegistrationMsg = 'Sorry, something went wrong';
+    });
+  }
+
+  createform() {
+    this.registerForm = this.fg.group({
+      firstName: [ '', Validators.required ],
+      middleName: [ '', Validators.required ],
+      lastName: [ '', Validators.required ],
+      login: [ '', Validators.required ],
+      email: [ '', Validators.required ],
+      password: [ '', Validators.required ],
+      repeatPassword: [ '', Validators.required ],
+      language: [ '', Validators.required ],
+      phoneNumber: [ '', Validators.required ],
+      birthDay: [ '', Validators.required ],
+      gender: [ '', Validators.required ],
+      level: [ '', Validators.required ],
+      community: [ '', Validators.required ],
+      region: [ '', Validators.required ],
+      nation: [ '', Validators.required ],
+
+    });
+  }
 }
