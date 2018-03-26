8cde7b6989b6bed1b2d59f272bcbae17b16ea658
Niroj Dyola <nirojdyola@gmail.com>
2018-03-26 17:04:50 +0545

    [#486] Join/Leave added in index section

--- src/app/meetups/meetups.component.ts
+++ src/app/meetups/meetups.component.ts
@@ -6,6 +6,11 @@ import { PlanetMessageService } from '../shared/planet-message.service';
 import { filterSpecificFields } from '../shared/table-helpers';
 import { SelectionModel } from '@angular/cdk/collections';
 import { Router } from '@angular/router';
+import { forkJoin } from 'rxjs/observable/forkJoin';
+import { UserService } from '../shared/user.service';
+import { findDocuments } from '../shared/mangoQueries';
+import { of } from 'rxjs/observable/of';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   templateUrl: './meetups.component.html',
@@ -28,7 +33,8 @@ export class MeetupsComponent implements OnInit, AfterViewInit {
     private couchService: CouchService,
     private dialog: MatDialog,
     private planetMessageService: PlanetMessageService,
-    private router: Router
+    private router: Router,
+    private userService: UserService
   ) { }
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -58,12 +64,12 @@ export class MeetupsComponent implements OnInit, AfterViewInit {
   }
 
   getMeetups() {
-    this.couchService.get('meetups/_all_docs?include_docs=true')
-      .subscribe((data) => {
-        // _all_docs returns object with rows array of objects with 'doc' property that has an object with the data.
-        // Map over data.rows to remove the 'doc' property layer
-        this.meetups.data = data.rows.map(meetup => meetup.doc);
-      }, (error) => this.planetMessageService.showAlert('There was a problem getting meetups'));
+    return this.couchService.get('meetups/_all_docs?include_docs=true');
+      // .subscribe((data) => {
+      //   // _all_docs returns object with rows array of objects with 'doc' property that has an object with the data.
+      //   // Map over data.rows to remove the 'doc' property layer
+      //   this.meetups.data = data.rows.map(meetup => meetup.doc);
+      // }, (error) => this.planetMessageService.showAlert('There was a problem getting meetups'));
   }
 
   deleteClick(meetup) {
@@ -142,13 +148,56 @@ export class MeetupsComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
-    this.getMeetups();
+    forkJoin(this.getMeetups(), this.getUserMeetups()).subscribe((results) => {
+      const meetupRes = results[0],
+        userMeetupRes = results[1];
+      this.setupList(meetupRes.rows, userMeetupRes.rows);
+    }, (err) => console.log(err));
     this.meetups.filterPredicate = filterSpecificFields([ 'title', 'description' ]);
+  }
+
+  setupList(meetupRes, userMeetupRes) {
+    this.meetups.data = meetupRes.map((m: any) => {
+      const meetup = m.doc || m;
+      const meetupIndex = userMeetupRes.findIndex(usermeetup => {
+        return (meetup._id === usermeetup.doc.meetupId && usermeetup.doc.memberId.indexOf(this.userService.get().name) > -1);
+      });
+      if (meetupIndex > -1) {
+        return { ...meetup, participate: true };
+      }
+      return { ...meetup,  participate: false };
+    });
   }
 
   updateMeetup(meetup) {
     const { _id: meetupId } = meetup;
     this.router.navigate([ '/meetups/update/' + meetup._id ]);
+  }
+
+  getUserMeetups() {
+    return this.couchService.get('usermeetups/_all_docs?include_docs=true');
+  }
+
+  attendMeetup(meetup) {
+    this.couchService.post(`usermeetups/_find`,
+      findDocuments({ 'meetupId': meetup._id }, 0 ))
+      .pipe(switchMap(data => {
+        const meetupInfo = { ...data.docs[0] };
+        const memberId = meetupInfo.memberId;
+        const username: string = this.userService.get().name;
+        (memberId.indexOf(username) > -1) ? memberId.splice(memberId.indexOf(username), 1) : memberId.push(username);
+        return this.couchService.put('usermeetups/' + meetupInfo._id , { ...meetupInfo, memberId });
+      })).subscribe((res) => {
+        const mData = this.meetups.data;
+        if (mData.length > 0) {
+          for (let i = 0; i < mData.length; i++ ) {
+            if (mData[i]['id'] === res._id) {
+              (this.meetups.data[i]['participate']) ?
+                this.meetups.data[i]['participate'] = false : this.meetups.data[i]['participate'] = true;
+            }
+          }
+        }
+      });
   }
 
 }
